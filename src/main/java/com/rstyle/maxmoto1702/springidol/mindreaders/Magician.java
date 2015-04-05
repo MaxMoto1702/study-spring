ec442d507d3a0699e1cf2112313010a991334d90
Max Serebryanskiy <Maksim.Serebryanskiy@R-Style.com>
2015-04-05 14:55:34 +0300

    5.4.2. Передача аргументов аннотированным советам

--- src/main/java/com/rstyle/maxmoto1702/springidol/mindreaders/Magician.java
+++ src/main/java/com/rstyle/maxmoto1702/springidol/mindreaders/Magician.java
@@ -1,21 +1,31 @@
 package com.rstyle.maxmoto1702.springidol.mindreaders;
 
 import com.rstyle.maxmoto1702.springidol.MindReader;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * Created by m on 05.04.2015.
  */
+@Aspect
 public class Magician implements MindReader {
 
     private static final Logger LOG = LoggerFactory.getLogger(Magician.class);
     private String thoughts;
 
+    @Pointcut("execution(* com.rstyle.maxmoto1702.springidol.Thinker.thinkOfSomething(String)) && args(thoughts)")
+    public void thinking(String thoughts) {
+
+    }
+
+    @Before("thinking(thoughts)")
     @Override
-    public void interceptThoughts(String thought) {
+    public void interceptThoughts(String thoughts) {
         LOG.info("Intercepting volunteer's thoughts");
-        this.thoughts = thought;
+        this.thoughts = thoughts;
     }
 
     @Override
