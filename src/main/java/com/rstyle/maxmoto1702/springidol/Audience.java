1fbb25450fecc1ea31a61e505b8e699e7d83901c
Max Serebryanskiy <Maksim.Serebryanskiy@R-Style.com>
2015-04-05 14:42:27 +0300

    5.4. Аннотирование аспектов

--- src/main/java/com/rstyle/maxmoto1702/springidol/Audience.java
+++ src/main/java/com/rstyle/maxmoto1702/springidol/Audience.java
@@ -1,38 +1,49 @@
 package com.rstyle.maxmoto1702.springidol;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * Created by m on 04.04.2015.
  */
+@Aspect
 public class Audience {
 
     private static final Logger LOG = LoggerFactory.getLogger(Audience.class);
 
+    @Pointcut("execution(* com.rstyle.maxmoto1702.springidol.Performer.perform(..))")
+    public void perfomance() {
+
+    }
+
+    @Before("perfomance()")
     public void takeSeats() {
         LOG.info("The audience is taking their seats.");
     }
 
+    @Before("perfomance()")
     public void turnOffCellPhones() {
         LOG.info("The audience is turning off their cellphones");
     }
 
+    @AfterReturning("perfomance()")
     public void applaud() {
         LOG.info("CLAP CLAP CLAP CLAP CLAP");
     }
 
+    @AfterThrowing("perfomance()")
     public void demandRefund() {
         LOG.info("Boo! We want our money back!");
     }
 
-    public void watchPerformance(ProceedingJoinPoint joinpoint) {
+    public void watchPerformance(ProceedingJoinPoint joinPoint) {
         try {
             LOG.info("The audience is taking their seats.");
             LOG.info("The audience is turning off their cellphones");
             long start = System.currentTimeMillis();
-            joinpoint.proceed();
+            joinPoint.proceed();
             long end = System.currentTimeMillis();
             LOG.info("CLAP CLAP CLAP CLAP CLAP");
             LOG.info("The performance took " + (end - start) + " milliseconds.");
